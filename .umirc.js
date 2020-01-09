83b930b940c2d4df98c912a0efbee1a9aa54e150
feifei1030 <964668506@qq.com>
2020-01-09 16:55:50 +0800

    修复quill的bug

--- .umirc.js
+++ .umirc.js
@@ -1,7 +1,23 @@
 
+const webpack = require('webpack')
+
 // ref: https://umijs.org/config/
 export default {
-  treeShaking: true,
+  treeShaking: true,  
+
+  // react-quill issue  https://github.com/zenoamaro/react-quill/issues/224
+  // 语法参考 https://github.com/neutrinojs/webpack-chain
+  chainWebpack: (config) => {
+    config
+      .plugin("ProvidePlugin")
+      .use(webpack.ProvidePlugin, [{        
+        'window.Quill': 'quill',
+        'Quill': 'quill'
+      }])
+
+    config.module.rule('js').exclude.add(/node_modules(?!\/quill-image-drop-module|quill-image-resize-module)/)
+  },
+
   routes: [
     {
       path: '/',
