6f5674732c84d1f3bcae9a0b72efce7936d1a382
feifei1030 <964668506@qq.com>
2020-01-09 16:59:09 +0800

    追加quill-drop

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,13 @@ import styles from './index.css';
 import 'react-quill/dist/quill.snow.css'; // ES6
 
 import ReactQuill, { Quill } from 'react-quill';
+import { ImageDrop } from 'quill-image-drop-module';
 import ImageResize from 'quill-image-resize-module';
+Quill.register('modules/ImageDrop', ImageDrop);
 Quill.register('modules/ImageResize', ImageResize);
 
 const modules = {
+  ImageDrop: true,
   ImageResize: {}
 };
 
