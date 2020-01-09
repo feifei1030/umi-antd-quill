12adde410c5a28724b272af4d670f7b2581b5d77
feifei1030 <964668506@qq.com>
2020-01-09 16:44:01 +0800

    简单引入react-quill

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,31 @@
+import React from 'react'
 import styles from './index.css';
+import ReactQuill from 'react-quill'; // ES6
+import 'react-quill/dist/quill.snow.css'; // ES6
+
+class Editor extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = { text: '' } // You can also pass a Quill Delta here
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(value) {
+    this.setState({ text: value })
+  }
+
+  render() {
+    return (
+      <ReactQuill value={this.state.text}
+                  onChange={this.handleChange} />
+    )
+  }
+}
 
 export default function() {
   return (
     <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li>
-          <a href="https://umijs.org/guide/getting-started.html">
-            Getting Started
-          </a>
-        </li>
-      </ul>
+      <Editor></Editor>
     </div>
   );
 }
